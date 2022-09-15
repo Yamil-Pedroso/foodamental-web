be5a608163792bd8eb71c51c24eec8d89504f093
Yami Pedroso-AI <yamirovinci@gmail.com>
2022-09-15 16:01:59 +0200

    working on recommendations page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,25 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action
 
   def home
     @user = current_user
   end
+
+  def recommendations
+    @user = current_user
+  end
+
+  def blogs
+    @user = current_user
+  end
+
+  def news
+    @user = current_user
+  end
+
+  def about
+    @user = current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'pages/recommendations'
+  get 'pages/blogs'
+  get 'pages/news'
+  get 'pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
